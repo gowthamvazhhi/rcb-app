815cad7e9cb480b6b45c87850068dc3b8f95a954
Gowtham VP <gautigowtham07@gmail.com>
2026-02-14 14:31:29 +0530

    team name change

--- src/main/java/com/ipl/rcb/controller/RcbController.java
+++ src/main/java/com/ipl/rcb/controller/RcbController.java
@@ -9,10 +9,10 @@ public class RcbController {
     
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("teamName", "Royal Challengers Bangalore");
+        model.addAttribute("teamName", "Royal Challengers Bengaluru");
         model.addAttribute("teamShortName", "RCB");
         model.addAttribute("founded", "2008");
-        model.addAttribute("homeGround", "M. Chinnaswamy Stadium, Bangalore");
+        model.addAttribute("homeGround", "M. Chinnaswamy Stadium, Bengaluru");
         model.addAttribute("captain", "Rajat Patidar"); // Updated captain as requested
         model.addAttribute("coach", "Andy Flower");
         model.addAttribute("titles", "1"); // Updated title count as requested
